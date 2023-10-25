55605e62af5989ed95e2f2eb73861dd18b6fbc30
sothatc <dkdltbem37@gmail.com>
2023-10-25 21:28:57 +0900

    [Fix] task user name fix

---
+++ src/main/java/itfact/entp/task/dto/TaskDTO.java
@@ -0,0 +1,30 @@
+package itfact.entp.task.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TaskDTO {
+
+//    private int task_unq;
+//    private int entp_unq;
+//    private String task_dept;
+//    TASK_UNQ
+//            ENTP_UNQ
+//    TASK_DEPT
+//            TASK_USR_NM
+//    TASK_TP
+//            TASK_JOB_TP
+//    SVC_EFC
+//            CUST_UNQ
+//    TASK_ST_DT
+//            TASK_ST_TM
+//    TASK_ED_DT
+//            TASK_ED_TM
+//    TASK_MEMO
+//            REG_DTM
+//    DEL_YN
+//            STT_MONTH_CNT
+//    STT_DAY_CNT
+}
--- src/main/java/itfact/entp/task/dto/TaskMembDTO.java
+++ src/main/java/itfact/entp/task/dto/TaskMembDTO.java
@@ -7,12 +7,12 @@ import lombok.Data;
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TaskMembDTO {
-    private int    memb_unq;
-    private String memb_dept_nm;
-    private String memb_nm;
-    private String memb_pst_nm;
-    private String memb_tel;
-    private String memb_email;
+    private int    task_usr_unq;
+    private String task_dept;
+    private String task_usr_nm;
+    private String task_usr_pst_nm;
+    private String task_usr_tel;
+    private String task_usr_email;
     private String reg_usr_id;
     private String reg_dtm;
     private String chg_usr_id;
